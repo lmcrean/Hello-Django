ce2cb8e2428d00e4698a21181167751ced7c4b94
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-10-27 22:08:35 +0100

    Add STATIC_ROOT to settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -134,3 +134,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STATIC_ROOT: This will define where the collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
